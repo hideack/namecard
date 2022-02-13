ed6f5f47e01af4b3296d26b810fbf2d976c06834
Hideaki Terai <hideack@pepabo.com>
2022-02-13 16:18:50 +0900

    API呼び出しをmodelに移動

--- app/controllers/keychains_controller.rb
+++ app/controllers/keychains_controller.rb
@@ -22,58 +22,17 @@ class KeychainsController < ApplicationController
 
   # POST /keychains or /keychains.json
   def create
-    require 'mini_magick'
-    image = MiniMagick::Image.open("./app/assets/images/860x860.png")
+    @keychain = Keychain.new(keychain_params)
 
-    image.combine_options do |config|
-      config.font "./app/assets/fonts/VL-PGothic-Regular.ttf"
-      config.pointsize 48
-      config.fill "#ff0000"
-      config.gravity "center"
-      config.draw "text 0,0 Hello!SUZURI"
+    respond_to do |format|
+      if @keychain.publish
+        format.html { redirect_to keychains_url, notice: "Keychain was successfully created." }
+        format.json { render :show, status: :created, location: @keychain }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @keychain.errors, status: :unprocessable_entity }
+      end
     end
-
-    temp_name = "#{SecureRandom.hex}"
-    temp_file = "./storage/#{temp_name}.png"
-    temp_url  = "https://suzuri-namecard.herokuapp.com/image/#{temp_name}"
-    image.write temp_file
-
-    logger.debug(temp_file)
-    logger.debug(temp_url)
-
-    token = get_access_token
-    record = {
-      :texture => temp_url,
-      :title => "れんしゅうそのいち",
-      :price => 2000,
-      :description => "API経由で作成",
-      :products => [
-        :itemId => 147,
-        :exemplaryItemVariantId => 1952,
-        :published => true,
-        :resizeMode => "contain",
-      ]
-    }
-
-    response = token.post(
-      "https://suzuri.jp/api/v1/materials",
-      {:body => record.to_json, :headers => {'Authorization' => "Bearer #{token.token}", 'Content-Type' => 'application/json'}}
-    )
-
-    logger.debug(response.status)
-    logger.debug(response.body)
-
-#    @keychain = Keychain.new(keychain_params)
-#
-#    respond_to do |format|
-#      if @keychain.save
-#        format.html { redirect_to keychain_url(@keychain), notice: "Keychain was successfully created." }
-#        format.json { render :show, status: :created, location: @keychain }
-#      else
-#        format.html { render :new, status: :unprocessable_entity }
-#        format.json { render json: @keychain.errors, status: :unprocessable_entity }
-#      end
-#    end
   end
 
   # PATCH/PUT /keychains/1 or /keychains/1.json
--- app/models/keychain.rb
+++ app/models/keychain.rb
@@ -1,2 +1,48 @@
 class Keychain < ApplicationRecord
+  def publish
+    require 'mini_magick'
+    image = MiniMagick::Image.open("./app/assets/images/860x860.png")
+
+    image.combine_options do |config|
+      config.font "./app/assets/fonts/VL-PGothic-Regular.ttf"
+      config.pointsize 48
+      config.fill "#ff0000"
+      config.gravity "center"
+      config.draw "text 0,0 Hello!SUZURI"
+    end
+
+    temp_name = "#{SecureRandom.hex}"
+    temp_file = "./storage/#{temp_name}.png"
+    temp_url  = "https://suzuri-namecard.herokuapp.com/image/#{temp_name}"
+    image.write temp_file
+
+    logger.debug(temp_file)
+    logger.debug(temp_url)
+
+    if Rails.env == 'production'
+      token = get_access_token
+      record = {
+        :texture => temp_url,
+        :title => "れんしゅうそのいち",
+        :price => 2000,
+        :description => "API経由で作成",
+        :products => [
+          :itemId => 147,
+          :exemplaryItemVariantId => 1952,
+          :published => true,
+          :resizeMode => "contain",
+        ]
+      }
+
+      response = token.post(
+        "https://suzuri.jp/api/v1/materials",
+        {:body => record.to_json, :headers => {'Authorization' => "Bearer #{token.token}", 'Content-Type' => 'application/json'}}
+      )
+
+      logger.debug(response.status)
+      logger.debug(response.body)
+    else
+      true
+    end
+  end
 end
