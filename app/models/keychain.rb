dd90f5cc90e2126f93760be40141caeefa6a45d2
Hideaki Terai <hideack@pepabo.com>
2022-02-13 21:58:37 +0900

    mini magick parameters changed.

--- app/models/keychain.rb
+++ app/models/keychain.rb
@@ -2,14 +2,15 @@ class Keychain < ApplicationRecord
   def publish(token)
     require 'mini_magick'
     image = MiniMagick::Image.open("./app/assets/images/860x860.png")
+
     logger.debug("MESSAGE = #{message}")
 
     image.combine_options do |config|
       config.font "./app/assets/fonts/VL-PGothic-Regular.ttf"
-      config.pointsize 48
+      config.pointsize 64
       config.fill "#ff0000"
       config.gravity "center"
-      config.draw "text 0,0 XYZ"
+      config.draw "text 0,0 #{message}"
     end
 
     temp_name = "#{SecureRandom.hex}"
