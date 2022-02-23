e1f9a8c366767bc9baf6bafc6304d6d1bd52210c
Hideaki Terai <hideack@pepabo.com>
2022-02-23 18:21:38 +0900

    before_action追記

--- app/controllers/keychains_controller.rb
+++ app/controllers/keychains_controller.rb
@@ -65,6 +65,7 @@ class KeychainsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_keychain
       @keychain = Keychain.find(params[:id])
+      @suzuri_response = nil
     end
 
     # Only allow a list of trusted parameters through.
