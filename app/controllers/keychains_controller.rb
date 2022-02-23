c090d6c0ad7cee208598425f694378f1c36f4de4
Hideaki Terai <hideack@pepabo.com>
2022-02-23 18:04:21 +0900

    SUZURI APIからの戻り値の渡し方を修正

--- app/controllers/keychains_controller.rb
+++ app/controllers/keychains_controller.rb
@@ -26,7 +26,9 @@ class KeychainsController < ApplicationController
     token = get_access_token
 
     respond_to do |format|
-      if @keychain.publish(token)
+      @suzuri_response = @keychain.publish(token)
+
+      if @suzuri_response
         format.html { redirect_to keychains_url, notice: "Keychain was successfully created." }
         format.json { render :show, status: :created, location: @keychain }
       else
--- app/models/keychain.rb
+++ app/models/keychain.rb
@@ -46,10 +46,12 @@ class Keychain < ApplicationRecord
         {:body => record.to_json, :headers => {'Authorization' => "Bearer #{token.token}", 'Content-Type' => 'application/json'}}
       )
 
-      @suzuri_response = JSON.parse(response.body)
+      suzuri_response = JSON.parse(response.body)
 
       logger.debug(response.status)
       logger.debug(@suzuri_response)
+
+      suzuri_response
     else
       true
     end
